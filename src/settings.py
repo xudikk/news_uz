1505ca2d322b7441639a95c2532ef7df8b8c61b0
xudikk <xudikk.1@gmail.com>
2025-09-25 16:36:34 +0500

    new files
    .env and settings
    requirements.txt added

--- src/settings.py
+++ src/settings.py
@@ -9,20 +9,21 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 APP_NAME = "News"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z^ro4zdu3x)3+b=18#x(3_xvjorzlg@-dr%yt8w49iasms+wh5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,17 +137,17 @@ MEDIA_ROOT = BASE_DIR /  'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TG_TOKEN = "1998469854:AAE8DgBdtWguqGOas0BKOB8qlYQgk8sF1vw"
+TG_TOKEN = os.getenv("TG_TOKEN")
 
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'xudikk.1@gmail.com'  # from_email
-EMAIL_HOST_PASSWORD = 'ccgvmklrmgdsffus'  # Gmail uchun AppPassword | gmail paroli emass!!!!
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # from_email
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # Gmail uchun AppPassword | gmail paroli emass!!!!
 
-ESKIZ_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTk1NzcyOTEsImlhdCI6MTc1Njk4NTI5MSwicm9sZSI6InRlc3QiLCJzaWduIjoiNWRjY2M5ZGE1MjNjZmI3OWI4YWY1NzRlOGZmNzY2OTAyYTE0NzAwOWMyNDg2ZWQ4MmMzNWRhY2RjNTdhYWY3MSIsInN1YiI6IjIzOTEifQ.hq3QCptZDsBMQypME-AJ8vup2oUfUQ6l_rCmTSpD1hQ"
+ESKIZ_TOKEN = os.getenv("ESKIZ_TOKEN")
 
 
 
